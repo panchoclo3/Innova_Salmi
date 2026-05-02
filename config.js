3736a865d49641b334594d51dfcb67b8eb43f86b
Francisco Maureira <franciscomaureirap@gmail.com>
2026-05-02 05:06:04 +0000

    primer commit

---
+++ config.js
@@ -0,0 +1,12 @@
+Protobject.setProduction(true)
+Protobject.initialize([
+  { 
+    name: "Conexion Juguete",
+    page: "arduino.html",
+  },
+  { 
+    name: "Interfaz Visual",
+    page: "index.html",
+    main: true,
+  }
+]);
